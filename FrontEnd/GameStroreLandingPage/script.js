422d767354b38da05599a7fcfe04d25d6397922b
UditNova <uditp1552@gmail.com>
2024-01-14 21:35:35 +0530

    Project update(gamestorelandingpage)

--- FrontEnd/GameStroreLandingPage/script.js
+++ FrontEnd/GameStroreLandingPage/script.js
@@ -1,10 +1,25 @@
 var crsr = document.querySelector(".cursor");
 var blur = document.querySelector(".cursor-blur");
+
 document.addEventListener("mousemove", function (dets) {
   crsr.style.left = dets.x + "px";
   crsr.style.top = dets.y + "px";
   blur.style.left = dets.x - 250 + "px";
   blur.style.top = dets.y - 250 + "px";
+});
+
+var h4all = document.querySelectorAll(".nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #95C11E";
+    crsr.style.backgroundColor = "#95C11E";
+  });
 });
 
 gsap.to(".nav", {
@@ -30,5 +45,57 @@ gsap.to(".main", {
     start: "top -25%",
     end: "top -70%",
     scrub: 2,
+  },
+});
+
+gsap.from(".about-us img, #about-us-in", {
+  y: 90,
+  opecity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".about-us",
+    scroller: "body",
+    start: "top 60%",
+    end: "top 55%",
+    scrub: 3,
+  },
+});
+
+gsap.from(".card", {
+  scale: 0.8,
+  opecity: 0,
+  duration: 1,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: ".card",
+    scroller: "body",
+    start: "top 60%",
+    end: "top 55%",
+    scrub: 1,
+  },
+});
+
+gsap.from("#colon1", {
+  y: 70,
+  x: 70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    //marker: true,
+    start: "top 55%",
+    end: "top 55%",
+    scrub: 4,
+  },
+});
+gsap.from("#colon2", {
+  y: -70,
+  x: -70,
+  scrollTrigger: {
+    trigger: "#colon2",
+    scroller: "body",
+    // marker: true,
+    start: "top 55%",
+    end: "top 55%",
+    scrub: 4,
   },
 });
